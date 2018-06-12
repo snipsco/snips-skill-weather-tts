dfa536c6f67ca89996a0bd7a868729e4fb5f3031
Charles <nebuto@users.noreply.github.com>
2018-06-12 21:17:34 +0200

    Quickfix for unrecognized intent

--- action-weatherIntentTtsParser-weather.py
+++ action-weatherIntentTtsParser-weather.py
@@ -21,7 +21,7 @@ def intent_received(hermes, intent_message):
         print('searchWeatherForecastItem')
         sentence += 'the weather '
     else:
-        hermes.publish_end_session(intent_message.session_id)
+        hermes.publish_end_session(intent_message.session_id, "I did not understand what you said")
         return
 
     forecast_country_slot = intent_message.slots.forecast_country.first()
